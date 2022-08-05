9503025f85437d1fefe77913c9a7b4b681d6f323
stray-awakening <1310290395@qq.com>
2022-08-05 23:33:41 +0800

    练习3

---
+++ Project3/Project3/test3.c
@@ -0,0 +1,146 @@
+ #define _CRT_SECURE_NO_WARNINGS
+ #include<stdio.h>
+//判断一个数是否为基数
+int main()
+{
+	//int num1 = 0;
+	//scanf("%d", &num1);
+	//if (1 == num1 % 2)
+	//	printf("%d是奇数\n", num1);
+	//else
+	//	printf("%d不是奇数\n", num1);
+	//int num2 = 0;
+
+//打印100以内的奇数
+	//while (num2 <= 100)
+	//{
+	//	if (1 == num2 % 2)
+	//		printf("%d ", num2);
+	//	num2 += 1;
+	//}
+
+	//给定两个整形变量的值，将两个值的内容进行交换
+	//int a = 0;
+	//int b = 0;
+	//int c = 0;
+	//scanf("%d %d", &a, &b);
+	//c = a;
+	//a = b;
+	//b = c;
+
+	//求10 个整数中最大值
+	//int num1 = 0;
+	//int num2 = 0;
+	//int num3 = 0;
+	//int num4 = 0;
+	//int num5 = 0;
+	//int num6 = 0;
+	//int num7 = 0;
+	//int num8 = 0;
+	//int num9 = 0;
+	//int num10 = 0;
+	//scanf("%d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10);
+
+	//int Max = 0;
+
+	//if (num1 > num2)
+	//	Max = num1;
+	//else
+	//	Max = num2;
+
+	//if (Max > num3)
+	//	Max = Max;
+	//else
+	//	Max = num3;
+
+	//if (Max > num4)
+	//	Max = Max;
+	//else
+	//	Max = num4;
+
+	//if (Max > num5)
+	//	Max = Max;
+	//else
+	//	Max = num5;
+
+	//if (Max > num6)
+	//	Max = Max;
+	//else
+	//	Max = num6;
+
+	//if (Max > num7)
+	//	Max = Max;
+	//else
+	//	Max = num7;
+
+	//if (Max > num8)
+	//	Max = Max;
+	//else
+	//	Max = num8;
+
+	//if (Max > num9)
+	//	Max = Max;
+	//else
+	//	Max = num9;
+
+	//if (Max > num10)
+	//	Max = Max;
+	//else
+	//	Max = num10;
+	//printf("%d", Max);
+
+	//将三个数按从大到小输出。
+	//int num1 = 0;
+	//int num2 = 0;
+	//int num3 = 0;
+	//scanf("%d %d %d", &num1, &num2, &num3);
+	//int nuber1 = 0;
+	//int nuber2 = 0;
+	//int nuber3 = 0;
+	//if (num1 > num2 && num1 > num3)
+	//	nuber1 = num1;
+	//else if (num2 > num1 && num2 > num3)
+	//	nuber1 = num2;
+	//else
+	//	nuber1 = num3;
+
+	//if (nuber1 == num1)
+	//{
+	//	if (num2 > num3)
+	//	{
+	//		nuber2 = num2;
+	//		nuber3 = num3;
+	//	}
+	//	else
+	//	{
+	//		nuber2 = num3;
+	//		nuber3 = num2;
+	//	}
+	//}
+	//else if (nuber1 == num2)
+	//{
+	//	if (num1 > num3)
+	//	{
+	//		nuber2 = num1;
+	//		nuber3 = num3;
+	//	}
+	//	else
+	//	{
+	//		nuber2 = num3;
+	//		nuber3 = num1;
+	//	}
+	//}
+	//else if (nuber1 == num3)
+	//{
+	//	if (num1 > num2)
+	//	{
+	//		nuber2 = num1;
+	//		nuber3 = num2;
+	//	}
+	//	else
+	//	{
+	//		nuber2 = num2;
+	//		nuber3 = num1;
+	//	}
+	//}
+	//printf("%d %d %d\n", nuber1, nuber2, nuber3);
